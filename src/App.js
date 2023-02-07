3396416de23ac14c0fd85ade8754e90c4592b22d
simon <simon.lu@tron.network>
2023-02-07 14:31:19 +0800

    remove basename

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import React from 'react';
+import React from "react";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
-import './assets/styles/App.css';
-import Question from './components/Question';
-import Layout from 'antd/es/layout/layout';
-import AppHeader from './components/Header';
-import QuestionList from './components/QuestionList';
-import PageNotFound from './components/PageNotFound';
+import "./assets/styles/App.css";
+import Question from "./components/Question";
+import Layout from "antd/es/layout/layout";
+import AppHeader from "./components/Header";
+import QuestionList from "./components/QuestionList";
+import PageNotFound from "./components/PageNotFound";
 
 function App() {
   return (
-    <BrowserRouter basename='geek-contract'>
+    <BrowserRouter>
       <Layout>
         <AppHeader />
         <div className="main flex">
           <Routes>
-            <Route path='/' element={<QuestionList />} />
-            <Route path='/questions/:questionId' element={<Question />} />
+            <Route path="/" element={<QuestionList />} />
+            <Route path="/questions/:questionId" element={<Question />} />
             <Route element={<PageNotFound />} />
           </Routes>
         </div>
