ad1bc401398508fbe550e278873f5675fc09b1ec
simon <simon.lu@tron.network>
2023-02-08 14:25:04 +0800

    build for btfs gateway

--- src/components/PageNotFound.js
+++
@@ -1,8 +0,0 @@
-const PageNotFound = (location) => {
-    return (
-        <p>Page not found - the path, <code>{location.pathname}</code>, did not match
-            any React Router routes.</p>
-    )
-}
-
-export default PageNotFound;
